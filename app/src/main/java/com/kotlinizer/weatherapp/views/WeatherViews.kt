4ae52263e7de55f87e1f6e3a50ce9f2ad035f100
Mike Reynolds <mikereynolds@carfax.com>
2022-12-03 06:57:23 -0600

    Added animation

--- app/src/main/java/com/kotlinizer/weatherapp/views/WeatherViews.kt
+++ app/src/main/java/com/kotlinizer/weatherapp/views/WeatherViews.kt
@@ -2,6 +2,7 @@ package com.kotlinizer.weatherapp.views
 
 import android.graphics.Typeface
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,12 +10,10 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -29,6 +28,12 @@ import com.kotlinizer.weatherapp.util.dayOfWeek
 
 @Composable fun CurrentWeather(weather: MutableState<WeatherData?>) {
 	val currentWeather = weather.value!!.list.first()
+	val infiniteTransient = rememberInfiniteTransition()
+	val offset by infiniteTransient.animateFloat(
+		initialValue = 1000f, targetValue = 2500f, animationSpec = infiniteRepeatable(
+			tween(10000, easing = LinearEasing), repeatMode = RepeatMode.Reverse
+		)
+	)
 	Box(
 		modifier = Modifier
 			.fillMaxWidth()
@@ -36,7 +41,7 @@ import com.kotlinizer.weatherapp.util.dayOfWeek
 				brush = Brush.linearGradient(
 					colors = listOf(
 						ComposeColors.DENIM, ComposeColors.DENIM, ComposeColors.BOMBAY
-					)
+					), end = Offset(0f, 0f), start = Offset(150f, offset)
 				)
 			)
 	) {
